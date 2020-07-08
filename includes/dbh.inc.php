30aaf825c4ce023f71c2803f8a76be7b90a6fa74
happypaws <58398246+happypaws@users.noreply.github.com>
2020-07-08 22:16:19 +0100

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,6 +1,6 @@
 <?php   //this file is database handler in includes folder
 
-    $servername = "lden1.mysql4.gear.host";  //because I am using XAMPP as local server the name of the variable is local server, 
+    $servername = "den1.mysql4.gear.host";  //because I am using XAMPP as local server the name of the variable is local server, 
     $dBUsername = "logindatabase11";                          //when using an external server, the name of that server needs to be added there
     $dBPassword = "Uq6ftwl4?-97";
     $dBName = "logindatabase11"; //the database name that I created in phpMyAdmin 
